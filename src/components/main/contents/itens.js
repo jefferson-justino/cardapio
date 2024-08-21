2bce1b4a342917ddcdf019ff945863e9b3e33b7e
Jeff <itachisucan@gmail.com>
2024-08-21 20:17:07 -0300

    update state

--- src/components/main/contents/itens.js
+++ src/components/main/contents/itens.js
@@ -3,16 +3,18 @@
 
 export default function Itens(props){
 
-    const produtos =[[{nome:'aqui', valor:0, imagem:''},{nome:'lá', valor:0, imagem:''}],
-                     [{nome:'', valor:0, imagem:''}],
-                     [{nome:'', valor:0, imagem:''}]]
+    const produtos =[[{nome:'bolo de chocolate', valor:10.00, imagem:''},{nome:'brigadeiro', valor:2.00, imagem:''}],
+                     [{nome:'coxinha', valor:1.00, imagem:''}],
+                     [{nome:'coca cola', valor:8.00, imagem:''}]]
+    
+
 
     let lista = produtos[props.index].map((item,index)=>{
         return(
             <article key={index}>
                  <img src="" alt="imagem do produto"/>
                 <h3>{item.nome}</h3>
-                 <p>{item.valor}</p>
+                 <p>{item.valor.toFixed(2)}</p>
             </article>
 
         ) 
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,14 +1,15 @@
+import { useState } from "react"
 import Itens from "./contents/itens"
 
 export default function Main (){
-    const index= 0
+    const [index, setIndex]= useState(0)
     return(
         <main>
             <nav>
                 <ul>
-                    <li><input type="button" value='Doces'/></li>
-                    <li><input type="button" value='Salgados'/></li>
-                    <li><input type="button" value='Bebidas'/></li>
+                    <li><input type="button" value='Doces' onClick={()=>setIndex(0)} /></li>
+                    <li><input type="button" value='Salgados' onClick={()=>setIndex(1)}/></li>
+                    <li><input type="button" value='Bebidas' onClick={()=>setIndex(2)}/></li>
                 </ul>
             </nav>
 
